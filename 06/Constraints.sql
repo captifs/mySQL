b876135f3e29be1f798d8cac7bf88d9297a204cf
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-26 19:46:54 +0300

    Update Constraints.sql

--- 06/Constraints.sql
+++ 06/Constraints.sql
@@ -1,3 +1,4 @@
+--default 
 ALTER TABLE customers
 CHANGE COLUMN number_of_complaints number_of_complaints INT DEFAULT 0;
 
@@ -9,3 +10,11 @@ SELECT * FROM customers;
 
 ALTER TABLE customers
 ALTER COLUMN number_of_complaints DROP DEFAULT;
+
+
+--not null
+
+ALTER TABLE companies
+CHANGE COLUMN company_name company_name VARCHAR(255) NOT NULL;
+ALTER TABLE companies
+MODIFY company_name VARCHAR(255) NULL;
