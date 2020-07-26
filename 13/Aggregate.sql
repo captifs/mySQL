b53db320b18b53b18cb10f24bdbcd43fbf45d604
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-26 23:39:53 +0300

    Update Aggregate.sql

--- 13/Aggregate.sql
+++ 13/Aggregate.sql
@@ -20,3 +20,22 @@ WHERE from_date > '1997-01-01';
 SELECT ROUND(AVG(SALARY),2)
 FROM salaries
 WHERE from_date > '1997-01-01';
+
+--Select the department number and name from the ‘departments_dup’ 
+--table and add a third column where you name the department number (‘dept_no’) as ‘dept_info’. If ‘dept_no’ does not have a value, use ‘dept_name’.
+SELECT dept_no,
+dept_name,
+COALESCE(dept_no,dept_name) AS dept_info 
+FROM departments
+ORDER BY dept_no ASC;
+
+
+
+--Modify the code obtained from the previous exercise in the following way. Apply the IFNULL() function to the values from the first and second column, 
+--so that ‘N/A’ is displayed whenever a department number has no value, and ‘Department name not provided’ is shown if there is no value for ‘dept_name’.
+
+SELECT IFNULL(dept_no, 'N/A') as dept_no,
+IFNULL(dept_name, 'Department name not provided') AS dept_name,
+COALESCE(dept_no, dept_name) AS dept_info
+FROM departments
+ORDER BY dept_no ASC;
