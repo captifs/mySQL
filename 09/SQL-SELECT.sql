f994fd37aeb40c3530b105f1ef372f187dd77259
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-26 21:26:08 +0300

    Update SQL-SELECT.sql

--- 09/SQL-SELECT.sql
+++ 09/SQL-SELECT.sql
@@ -72,3 +72,10 @@ WHERE salary >= 150000;
 SELECT DISTINCT hire_date
 FROM employees;
 
+                       --How many annual contracts with a value higher than or equal to $100,000 have been registered in the salaries table?
+SELECT COUNT(*) FROM salaries
+
+WHERE salary >= 100000;
+
+--How many managers do we have in the “employees” database? Use the star symbol (*) in your code to solve this exercise.
+SELECT COUNT(*) FROM dept_manager;
