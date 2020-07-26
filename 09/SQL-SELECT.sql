7ac9012b91caf0fcf8105d9ca0a0263d4c3b5977
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-26 21:47:52 +0300

    Update SQL-SELECT.sql

--- 09/SQL-SELECT.sql
+++ 09/SQL-SELECT.sql
@@ -96,4 +96,16 @@ GROUP BY BY salary;
 ----Select all employees whose average salary is higher than $120,000 per annum.
 SELECT emp_no,AVG(SALARY) FROM salaries
 GROUP BY emp_no
+ORDER BY emp_no;    
+                       
+--Select the employee numbers of all individuals who have signed more than 1 contract after the 1st --of January 2000.
+--Hint: To solve this exercise, use the “dept_emp” table.
+SELECT emp_no FROM dept_emp
+
+WHERE from_date >= '2000-01-01'
+
+GROUP BY emp_no
+
+HAVING COUNT(from_date) > 1
+
 ORDER BY emp_no;                       
