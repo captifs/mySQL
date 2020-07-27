6751c2f5ccf9af0444a51dfbb29c25b9295b2702
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-27 08:41:49 +0300

    Update Joins.sql

--- 14/Joins.sql
+++ 14/Joins.sql
@@ -2,8 +2,9 @@ CREATE TABLE dept_dup (dept_no CHAR(4),
 dept_name VARCHAR(40));
 
 INSERT INTO dept_dup
-SELECT * FROM departments
-
+SELECT * FROM departments;
+                  
+           
                   
 
 CREATE TABLE dept_manager_dup
@@ -13,4 +14,12 @@ from_date date NOT NULL,
 to_date date NULL);
 
 INSERT INTO dept_manager_dup
-SELECT * FROM dept_manager
+SELECT * FROM dept_manager;
+                  
+                  
+                  
+--Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date.    
+ SELECT E.emp_no, E.first_name, E.last_name, M.dept_no,M.from_date
+FROM employees E
+INNER JOIN  dept_manager M
+ON E.emp_no = M.emp_no
