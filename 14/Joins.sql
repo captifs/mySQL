7a114dffb80ec22cdd02e7270eeabbb0e2e63d90
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-27 08:35:31 +0300

    Update Joins.sql

--- 14/Joins.sql
+++ 14/Joins.sql
@@ -4,12 +4,13 @@ dept_name VARCHAR(40));
 INSERT INTO dept_dup
 SELECT * FROM departments
 
+                  
 
-CREATE TABLE dept_manager_dup(
-emp_no INT(11) NOT NULL,
+CREATE TABLE dept_manager_dup
+(emp_no INT(11) NOT NULL,
 dept_no char(4) NULL,
-from_date NOT NULL,
-to_date NULL);
+from_date date NOT NULL,
+to_date date NULL);
 
 INSERT INTO dept_manager_dup
 SELECT * FROM dept_manager
