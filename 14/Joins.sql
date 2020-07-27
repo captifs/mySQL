63667c7ec99da2057148983c686d2c52f51052f5
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-27 08:33:09 +0300

    Create Joins.sql

---
+++ 14/Joins.sql
@@ -0,0 +1,15 @@
+CREATE TABLE dept_dup (dept_no CHAR(4),
+dept_name VARCHAR(40));
+
+INSERT INTO dept_dup
+SELECT * FROM departments
+
+
+CREATE TABLE dept_manager_dup(
+emp_no INT(11) NOT NULL,
+dept_no char(4) NULL,
+from_date NOT NULL,
+to_date NULL);
+
+INSERT INTO dept_manager_dup
+SELECT * FROM dept_manager
